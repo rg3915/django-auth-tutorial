10820478f9c57a005ce7ecd8574bd22982703440
rg3915 <regis42santos@gmail.com>
2021-04-08 18:46:54 -0300

    update core/views.py

--- myproject/core/views.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
-
-@login_required
-def index(request):
-    template_name = 'index.html'
-    return render(request, template_name)
