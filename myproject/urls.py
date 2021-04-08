0eff955d361875d49b6d24e4428686eb3304ef17
rg3915 <regis42santos@gmail.com>
2021-04-08 18:39:09 -0300

    Quebrando links e comentando urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('myproject.core.urls', namespace='core')),
+    # path('', include('myproject.core.urls', namespace='core')),
     # path('', include('django.contrib.auth.urls')),  # sem namespace
-    path('accounts/', include('myproject.accounts.urls')),  # sem namespace
+    # path('accounts/', include('myproject.accounts.urls')),  # sem namespace
     path('admin/', admin.site.urls),
 ]
