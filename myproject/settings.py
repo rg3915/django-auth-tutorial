13158312afda6dd9f30ae1bc57b61e19141c5f84
Regis Santos <regis42santos@gmail.com>
2022-02-09 22:30:11 -0300

    fix: accounts.Profile: (models.W042) Auto-created primary key used when not defining a primary key type. close #4

--- myproject/settings.py
+++ myproject/settings.py
@@ -127,3 +127,8 @@ STATIC_URL = '/static/'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'core:index'
 LOGOUT_REDIRECT_URL = 'core:index'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
