136b8f9834f4a070d07de981387bb9f04ac97470
Regis Santos <regis42santos@gmail.com>
2022-02-10 00:09:28 -0300

    fix: Atualiza AccountActivationTokenGenerator. close #5

--- myproject/accounts/tokens.py
+++ myproject/accounts/tokens.py
@@ -5,7 +5,7 @@ from django.utils import six
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (
-            six.text_type(user.pk) + six.text_type(timestamp)
+            six.text_type(user.pk) + six.text_type(timestamp) + six.text_type(user.email)  # noqa E501
         )
 
 
